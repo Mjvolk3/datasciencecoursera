a3d4c6d9c89e7633e9e1966b3318b63e71f45e35
Mjvolk3 <48566106+Mjvolk3@users.noreply.github.com>
2020-03-30 09:28:59 -0500

    Complete.R, checks for complete cases, corr.R, finding correlations between chemicals,completed

---
+++ complete.R
@@ -0,0 +1,42 @@
+#Week 2 part 2
+# Write a function that reads a directory full of files and reports the 
+# number of completely observed cases in each data file. The function 
+# should return a data frame where the first column is the name of the file 
+# and the second column is the number of complete cases.
+
+#example
+#complete("specdata", c(2, 4, 8, 10, 12))
+##   id nobs
+## 1  2 1041
+## 2  4  474
+## 3  8  192
+## 4 10  148
+## 5 12   96
+
+complete <- function(directory, id = 1:332){
+  
+  df_ret <- data.frame('id' = NULL, 'nobs' = NULL)
+  count <- 1
+  
+  for (i in id) {
+    
+    current_file <- (sprintf("%s/%03d.csv",directory,i))
+    df <- read.csv(current_file)
+    #Equivalent lines
+    #df[complete.cases(df[1:731,]),]
+    df_clean <-na.omit(df[,])
+    
+    nrow <- dim(df_clean)[1]
+    #build data frame
+    df_ret[count, 'id'] <- i
+    df_ret[count, 'nobs'] <- nrow
+    
+    count <- count + 1
+  } 
+  return(df_ret)
+}
+
+
+
+
+
---
+++ corr.R
@@ -0,0 +1,43 @@
+# #Week 2 part 2
+# #Write a function that takes a directory of data files and a 
+# threshold for complete cases and calculates the correlation between 
+# sulfate and nitrate for monitor locations where the number of 
+# completely observed cases (on all variables) is greater than the 
+# threshold. The function should return a vector of correlations for
+# the monitors that meet the threshold requirement. If no monitors meet
+# the threshold requirement, then the function should return a numeric 
+# vector of length 0.
+
+#example
+# cr <- corr("specdata", 150)
+# head(cr)
+## [1] -0.01895754 -0.14051254 -0.04389737 -0.06815956 -0.12350667 -0.07588814
+# summary(cr)
+##     Min.  1st Qu.   Median     Mean  3rd Qu.     Max. 
+## -0.21057 -0.04999  0.09463  0.12525  0.26844  0.76313
+
+# corr <- function(directory, threshold = 0){
+#   
+#   
+# }
+
+corr <- function(directory, threshold = 0){
+  
+comp_cases <- complete(directory, id = 1:332)
+thresh_cases <- subset(comp_cases, nobs>threshold)
+cor_mon <- vector()
+
+  if (dim(thresh_cases)[1] == 0){
+    cor_mon = 0
+  } else {
+    for(i in 1:nrow(thresh_cases)) {
+      current_id <- thresh_cases['id'][i,1]
+      current_file <- (sprintf("%s/%03d.csv",directory,current_id))
+      df <- read.csv(current_file)
+      df_clean <-na.omit(df[,])
+      cor_mon <- c(cor_mon, cor(df_clean["sulfate"], df_clean["nitrate"]))
+    } 
+  }
+
+  cor_mon
+}
