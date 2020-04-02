7173c4d95c3973b88a761b068143d86fee0f1610
Mjvolk3 <48566106+Mjvolk3@users.noreply.github.com>
2020-04-02 17:56:42 -0500

    cache vector example provided by coursera

---
+++ cache_vector.R
@@ -0,0 +1,28 @@
+#Cahcing a mean vector example for R programming Week 3 programming assignment
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
